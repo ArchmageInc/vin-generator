4c9008dcdebc79990816809073d7966270f20d18
ArchmageInc <Elminster2031@ArchmageInc.com>
2017-09-01 09:11:12 -0400

    Improved generation performance. Closes #1

--- vin-generator.js
+++ vin-generator.js
@@ -3,7 +3,9 @@ var _ = require('lodash'),
     prefixes = require('./prefixes.js');
 
 var vinDigitPositionMultiplier = [ 8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2 ];
-var vinDigitValues = { 'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H':8, 'J':1, 'K':2, 'L':3, 'M':4, 'N':5, 'P':7, 'R':9, 'S':2, 'T':3, 'U':4, 'V':5,'W':6, 'X':7, 'Y':8, 'Z':9, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '0':0};
+var vinDigitValues = { 'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H':8, 'J':1, 'K':2, 'L':3, 'M':4, 'N':5, 'P':7, 'R':9, 'S':2, 'T':3, 'U':4, 'V':5,'W':6, 'X':7, 'Y':8, 'Z':9, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '0':0},
+    prefixKeys = _.keys(prefixes),
+    vinDigitValueKeys = _.keys(vinDigitValues);
 
 function getCheckSum (vin) {
     var checkSumTotal = 0,
@@ -30,10 +32,10 @@ function getCheckSum (vin) {
 };
 
 function generateVin() {
-    var prefix = _.sample(_.keys(prefixes)),
+    var prefix = _.sample(prefixKeys),
         code = prefixes[prefix],
-        char = _.sample(_.keys(vinDigitValues)),
-        chars = _.times(7,_.partial(_.sample, _.keys(vinDigitValues))).join(''),
+        char = _.sample(vinDigitValueKeys),
+        chars = _.times(7,_.partial(_.sample, vinDigitValueKeys)).join(''),
         vinPart = prefix + char + code + chars,
         check = getCheckSum(vinPart);
 
